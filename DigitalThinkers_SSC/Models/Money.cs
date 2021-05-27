df11d9d14ea3516b6530db9f4c6b37fa4c0dc387
Koós Zoltán <koos.zoltan.iso@gmail.com>
2021-05-27 13:41:50 +0200

    Added Money class.
    Created Money class to represent the money in the self-checkout machine.

---
+++ DigitalThinkers_SSC/Models/Money.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalThinkers_SSC.Models
+{
+    public class Money
+    {
+        public long Id { get; set; }
+        public int Value { get; set; }
+        public int Volume { get; set; }
+    }
+}
