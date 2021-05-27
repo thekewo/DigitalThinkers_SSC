5926f6429abe55a23a886622cceeea6dd1b50540
Koós Zoltán <koos.zoltan.iso@gmail.com>
2021-05-27 13:51:16 +0200

    Added MoneyContext and connected to services. Cleaned up Startup class.
    MoneyContext represents a databse context that contains values of Money objects. Removed swagger usages from Startup class and added dbcontext to the services.

---
+++ DigitalThinkers_SSC/Models/MoneyContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalThinkers_SSC.Models
+{
+    public class MoneyContext : DbContext
+    {
+        public MoneyContext(DbContextOptions<MoneyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Money> MoneyItems { get; set; }
+    }
+}
--- DigitalThinkers_SSC/Startup.cs
+++ DigitalThinkers_SSC/Startup.cs
@@ -1,7 +1,9 @@
+using DigitalThinkers_SSC.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,29 +25,21 @@ namespace DigitalThinkers_SSC
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<MoneyContext>(opt =>
+                                               opt.UseInMemoryDatabase("MoneyList"));
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DigitalThinkers_SSC", Version = "v1" });
-            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DigitalThinkers_SSC v1"));
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
