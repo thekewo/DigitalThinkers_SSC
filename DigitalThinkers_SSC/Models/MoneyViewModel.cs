bc4d553e9315264b2bde07e0659abbdf077d6a24
Koós Zoltán <koos.zoltan.iso@gmail.com>
2021-05-27 11:45:59 +0200

    Created Database and added Money table creation sql script to project. Added DT_SSC_Database.mdf to gitignore.

---
+++ DigitalThinkers_SSC/Models/MoneyViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DigitalThinkers_SSC.Models
+{
+    public class MoneyViewModel
+    {
+        public Dictionary<string, int> inserted { get; set; }
+        public int price { get; set; }
+    }
+}
