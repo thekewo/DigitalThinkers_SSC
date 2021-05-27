df068268983563779276b0c6150bc30233bac59d
Koós Zoltán <koos.zoltan.iso@gmail.com>
2021-05-27 17:41:37 +0200

    Added Checkout method to V1Controller. Turned off browser launching. Plus small clean up.
    Removed some unnnecesary generated code. Added Checkout  method that gets the payment and price information, then calculates if the transaction is possible.

--- DigitalThinkers_SSC/Controllers/V1Controller.cs
+++ DigitalThinkers_SSC/Controllers/V1Controller.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DigitalThinkers_SSC.Models;
+using Microsoft.Extensions.Logging;
 
 namespace DigitalThinkers_SSC.Controllers
 {
@@ -14,19 +15,23 @@ namespace DigitalThinkers_SSC.Controllers
     public class V1Controller : ControllerBase
     {
         private readonly StockContext _context;
+        private readonly ILogger<V1Controller> _logger;
 
-        public V1Controller(StockContext context)
+        public V1Controller(
+            StockContext context,
+            ILogger<V1Controller> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         [HttpPost]
         public async Task<Dictionary<string,int>> PostStock(StockViewModel moneyViewModel)
         {
+            _logger.LogInformation("PostStock called at: {time}", DateTimeOffset.Now);
             foreach (var stock in moneyViewModel.inserted)
             {
                 var stockValue = int.Parse(stock.Key);
-                var sadsa = _context.StockItems.Select(s => s.Value);
                 if (_context.StockItems.Select(s => s.Value).Contains(stockValue))
                 {
                     var stockInDb = _context.StockItems.Find(stockValue);
@@ -45,7 +50,14 @@ namespace DigitalThinkers_SSC.Controllers
         [HttpGet]
         public async Task<Dictionary<string, int>> GetStock()
         {
-            var stockList = await _context.StockItems.ToListAsync();
+            Dictionary<string, int> stockDictionary = ListToDictionary(await _context.StockItems.ToListAsync());
+
+            return stockDictionary;
+        }
+
+        private Dictionary<string, int> ListToDictionary(List<Stock> stockList)
+        {
+            
             var stockDictionary = new Dictionary<string, int>();
             foreach (var stock in stockList)
             {
@@ -53,6 +65,53 @@ namespace DigitalThinkers_SSC.Controllers
             }
 
             return stockDictionary;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Checkout(StockViewModel moneyViewModel)
+        {
+            var returnedBillsAndCoins = new Dictionary<string, int>();
+
+            foreach (var stock in moneyViewModel.inserted)
+            {
+                moneyViewModel.price -= stock.Value * int.Parse(stock.Key);
+            }
+
+            var stockItems = _context.StockItems.ToArray();
+            Array.Sort(stockItems, (a, b) => b.Value.CompareTo(a.Value));
+            var isAllZeros = !stockItems.Select(s => s.Volume).Any(v => v != 0);
+
+            while (moneyViewModel.price < 0 && stockItems.Length > 0 && !isAllZeros)
+            {
+                foreach (var stock in stockItems)
+                {
+                    var addCounter = 0;
+                    while (stock.Volume > 0 && (Math.Abs(moneyViewModel.price) >= stock.Value))
+                    {
+                        moneyViewModel.price += stock.Value;
+                        stock.Volume--;
+                        addCounter++;
+                    }
+                    returnedBillsAndCoins.Add(stock.Value.ToString(), addCounter);
+                }
+                isAllZeros = !stockItems.Select(s => s.Volume).Any(v => v != 0);
+            }
+
+            if(moneyViewModel.price == 0)
+            {
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction(nameof(Checkout), returnedBillsAndCoins);
+            }
+
+            if(moneyViewModel.price > 0)
+            {
+                return BadRequest("Payment isn't sufficient.");
+            }
+            else
+            {
+                return BadRequest("Stock insufficient.");
+            }
         }
     }
 }
